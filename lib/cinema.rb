df961bb2348b563350960c4935f1f0cad161d00b
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-08-06 19:02:40 +0100

    refactoring Cinema Class - deleted methods not required and duplicate tests

--- lib/cinema.rb
+++ lib/cinema.rb
@@ -3,21 +3,7 @@ class Cinema
   def initialize(rows,seats)
     @rows  = rows
     @seats = seats
-    @auditorium = Array.new(@rows) { Array.new(@seats) }
-    @auditorium
-  end
-
-  # def create_cinema
-  #   auditorium = Array.new(@rows) { Array.new(@seats) }
-  #   auditorium
-  # end
-
-  def seats
-    @seats
-  end
-
-  def rows
-    @rows
+    @auditorium = Array.new(rows) { Array.new(seats) }
   end
 
   def auditorium
@@ -25,6 +11,3 @@ class Cinema
   end
 
 end
-
-    # auditorium = Array.new(@rows) { Array.new(@seats) }
-    # auditorium[0][0]=1
--- spec/cinema_spec.rb
+++ spec/cinema_spec.rb
@@ -6,12 +6,10 @@ let (:odeon) { Cinema.new(100,50) }
   context "set up" do
   
     it 'should have 50 seats in a row' do
-      expect(odeon.seats).to eq 50
       expect(odeon.auditorium[0].length).to eq 50
     end
 
     it 'should have 100 rows' do
-      expect(odeon.rows).to eq 100
       expect(odeon.auditorium.length).to eq 100
     end
 
