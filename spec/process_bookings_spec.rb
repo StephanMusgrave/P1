5ef239f6f0b43559116ddd7ac6b3d3c40015bd80
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-08-10 14:56:38 +0100

    process_bokkings method started

--- spec/process_bookings_spec.rb
+++ spec/process_bookings_spec.rb
@@ -4,7 +4,6 @@ require 'booking_request'
 
 describe "Processing a file of booking requests" do
   let (:odeon)    { Cinema.new(100,50) }
-  let (:validbooking) { BookingRequest.new(0,1,5,1,7) }
 
   context "getting booking requests from a file" do
 
@@ -28,6 +27,33 @@ describe "Processing a file of booking requests" do
       expect(request_list[499][2]).to eq 2
       expect(request_list[499][4]).to eq 4
     end
+
+  end
+
+  context "processing the bookings from the file file sample_booking_requests" do
+
+    it 'should read the booking requests from the file' do
+      empire = Cinema.new(100,50)
+      file = "data/sample_booking_requests"
+      process_bookings('data/sample_booking_requests',empire)
+      expect(empire.auditorium[0][0]).to eq 0
+      # expect()
+    end
+
+    xit 'should read, check and make each booking if valid' do
+      # request_list = get_bookings('data/sample_booking_requests')
+      # puts request_list[1]
+      # request_list.each { |booking| puts booking }
+      #   if check_booking(booking,odeon) == true
+      #     make_booking(booking,odeon)
+      #   else
+      #     puts "invalid booking"
+      #   end
+      # }      
+    end
+
+
+
 
   end
 
