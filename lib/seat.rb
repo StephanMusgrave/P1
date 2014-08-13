18fe4d50fa6767beb3f27743794cf6a3eb5ede5f
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-08-13 17:51:35 +0100

    create seat test passes

--- lib/seat.rb
+++ lib/seat.rb
@@ -1,3 +1,20 @@
 class Seat
 
+  def initialize
+    empty!
+  end
+
+  def booked?
+    @booked
+  end
+
+  def book!
+    @booked = true
+  end
+
+  def empty!
+    @booked = false
+  end
+
+
 end
--- spec/seat_spec.rb
+++ spec/seat_spec.rb
@@ -1,7 +1,11 @@
 require "seat"
 
 describe Seat do
+  let(:seat){ Seat.new}
 
+  it 'is empty when created' do
+    expect(Seat.new).not_to be_booked
+  end
 
 
 
